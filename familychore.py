c147d532c125addbfde4eeff0ff186d5c74e7f3a
PeterxKe <kemmeterpeter@gmail.com>
2026-06-28 11:55:25 +0200

    Update familychore.py

--- familychore.py
+++ familychore.py
@@ -4,7 +4,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("familychore-1f7d7-firebase-adminsdk-fbsvc-d57f1a3b51.json")
+    cred = credentials.Certificate(st.secrets["firebase_key"])
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://familychore-1f7d7-default-rtdb.europe-west1.firebasedatabase.app/"
     })
