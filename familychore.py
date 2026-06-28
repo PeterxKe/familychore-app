c6e92770c5a1a88752e64092daec48ec71fdf366
PeterxKe <kemmeterpeter@gmail.com>
2026-06-28 12:51:48 +0200

    Update familychore.py

--- familychore.py
+++ familychore.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import datetimeimport firebase_admin
+import datetime
+import firebase_admin
 from firebase_admin import credentials
 
 cred = credentials.Certificate("path/to/serviceAccountKey.json")
